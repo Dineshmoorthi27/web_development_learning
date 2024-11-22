a69f78df479db9801e7a3c6be245194c6a3f1e1d
Dineshmoorthi27 <dineshmoorthi27.gmail.com>
2024-11-22 18:38:39 +0530

    added database , local storage related js files

--- javascript/advance/database.js
+++ javascript/advance/database.js
@@ -5,7 +5,7 @@ let mysql = require('mysql');
 // Create connection object
 let con = mysql.createConnection({
     user:"root",
-    password:"root",
+    password:"Dinesh@22",
     database:"jsql",
     host:"localhost",
 })
@@ -21,31 +21,31 @@ con.connect((err)=>{
     
 })
 // creating table in mysql database
-// let query = "CREATE TABLE car (slno INTEGER(4),name VARCHAR(20),brand VARCHAR(20),model INTEGER(4),color VARCHAR(20))"
-// con.query(query,(err,res)=>{
-//     if(err){
-//         console.log("can't create table");
-//     }
-//     else{
-//         console.log("table created");
-//     }
+let query = "CREATE TABLE car (slno INTEGER(4),name VARCHAR(20),brand VARCHAR(20),model INTEGER(4),color VARCHAR(20))"
+con.query(query,(err,res)=>{
+    if(err){
+        console.log("can't create table");
+    }
+    else{
+        console.log("table created");
+    }
         
-// })
-// con.query('CREATE TABLE user (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255)')\
+})
+
 
 // insert data into table
-// let data = "INSERT INTO car(slno,name,brand,model,color)VALUES(2,'XUV500','Mahindra',2024,'Black')";
-// con.query(data,(err,res)=>{
-//     if(err){
-//         console.log("can't insert data");
-//         }
-//         else{
-//             console.log("data inserted",res.affectedRows);
-//             }
-// })
+let data = "INSERT INTO car(slno,name,brand,model,color)VALUES(2,'XUV500','Mahindra',2024,'Black')";
+con.query(data,(err,res)=>{
+    if(err){
+        console.log("can't insert data");
+        }
+        else{
+            console.log("data inserted",res.affectedRows);
+            }
+})
 // display the data
-let query = "SELECT * FROM car";
-con.query(query,(err,res)=>{
+let display = "SELECT * FROM car";
+con.query(display,(err,res)=>{
     if(err){
         console.log("can't display data");
         }
